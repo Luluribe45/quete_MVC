6358439f33437ec92fa60fbc21efaeedbd51c66e
Lucile varloteaux <varloteaux.lucile@gmail.com>
2018-10-04 17:29:39 +0200

    commit base mvc part 3

--- src/Model/ItemManager.php
+++ src/Model/ItemManager.php
@@ -12,6 +12,16 @@ class ItemManager
         $res = $pdo->query($query);
         return $res->fetchAll();
     }
+    public function selectOneItem(int $id) : array
+    {
+        $pdo = new \PDO(DSN, USER, PASS);
+        $query = "SELECT * FROM item WHERE id = :id";
+        $statement = $pdo->prepare($query);
+        $statement->bindValue(':id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+        // contrairement à fetchAll(), fetch() ne renvoie qu'un seul résultat
+        return $statement->fetch();
+    }
 
 
 }
